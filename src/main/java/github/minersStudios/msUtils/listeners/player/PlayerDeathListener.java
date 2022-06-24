29cfca2ccb1856bff6881f57fe47466f7893b2ba
Poloskun_ <nikongricisin339@gmail.com>
2022-06-24 16:55:04 +0300

    Updated version to 0.0.2 & now if a player killed another player a corresponding message will be displayed

--- src/main/java/github/minersStudios/msUtils/listeners/player/PlayerDeathListener.java
+++ src/main/java/github/minersStudios/msUtils/listeners/player/PlayerDeathListener.java
@@ -2,6 +2,7 @@ package github.minersStudios.msUtils.listeners.player;
 
 import github.minersStudios.msUtils.classes.PlayerInfo;
 import github.minersStudios.msUtils.classes.SitPlayer;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,19 +14,19 @@ public class PlayerDeathListener implements Listener {
 
     @EventHandler
     public void onPlayerDeath(@Nonnull PlayerDeathEvent event) {
-        Player player = event.getEntity();
-        PlayerInfo playerInfo = new PlayerInfo(player.getUniqueId());
-        SitPlayer sitPlayer = new SitPlayer(player);
-        event.setDeathMessage(
-                playerInfo.getGoldenName() + " "
-                + playerInfo.getPronouns().getDeathMessage()
-        );
-
+        Player killed = event.getEntity(), killer = event.getEntity().getKiller();
+        PlayerInfo killedInfo = new PlayerInfo(killed.getUniqueId());
+        SitPlayer sitPlayer = new SitPlayer(killed);
+        if(killer != null){
+            event.setDeathMessage(" " + new PlayerInfo(killer.getUniqueId()).getGoldenName() + ChatColor.of("#ffee93") + " убил " + killedInfo.getGoldenName());
+        } else {
+            event.setDeathMessage(" " + killedInfo.getGoldenName() + ChatColor.of("#ffee93") + " умер");
+        }
         if (sitPlayer.isSitting()) {
             sitPlayer.setSitting(null);
         }
-        if (playerInfo.hasPlayerDataFile()) {
-            playerInfo.setLastDeathLocation(player.getLocation());
+        if (killedInfo.hasPlayerDataFile()) {
+            killedInfo.setLastDeathLocation(killed.getLocation());
         }
     }
 }
